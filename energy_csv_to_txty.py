04aaf595b6f774091eeb5378753f58373ecf2aaf
eishitakalra <eishitakalra@outlook.com>
2025-03-02 11:38:14 +0000

    assignment 2 energy csv to txt for mosel data

--- energy_csv_to_txty.py
+++ energy_csv_to_txty.py
@@ -28,6 +28,22 @@ with open('A2_Operational_2015.txt', 'w') as file:
     for i in range(911,1583):
         file.write(f" {dem[i]} ")
     
-    file.write(' ]')
+    file.write(f' ] \n')
 
+    file.write('W_2015: [ ')
     
+    # Write data for each row
+    for i in range(911,1583):
+        file.write(f" {win[i]} ")
+    
+    file.write(f' ] \n')
+
+    file.write('S_2015: [ ')
+    
+    # Write data for each row
+    for i in range(911,1583):
+        file.write(f" {sol[i]} ")
+    
+    file.write(f' ] \n')
+
+

b41cd302ca6b746882d0cfe2f974f9089b4db17f
eishitakalra <eishitakalra@outlook.com>
2025-03-03 15:04:38 +0000

    Eergy A2 Planning data to mosel form code

--- energy_csv_to_txty.py
+++ energy_csv_to_txty.py
@@ -18,7 +18,7 @@ with open('Y15_v4.csv', newline='', encoding='utf-8') as csvfile:
         sol.append((row[4]) )
         airtemp.append((row[5])) 
         
-    print(f"Time: {t[0]}, Dem: {dem[0]}, Win: {win[0]}, Sol: {sol[0]}, AirTemp: {airtemp[0]}")
+    print(f"Time: {t[17519]}, Dem: {dem[17519]}, Win: {win[17519]}, Sol: {sol[17519]}, AirTemp: {airtemp[17519]}")
 
 with open('A2_Operational_2015.txt', 'w') as file:
 
@@ -42,6 +42,35 @@ with open('A2_Operational_2015.txt', 'w') as file:
     
     # Write data for each row
     for i in range(911,1583):
+        file.write(f" {sol[i]} ")
+    
+    file.write(f' ] \n')
+
+
+
+
+with open('A2_Planning_2015.txt', 'w') as file:
+
+    file.write('D: [ ')
+    
+    # Write data for each row
+    for i in range(0,17520):
+        file.write(f" {dem[i]} ")
+    
+    file.write(f' ] \n')
+
+    file.write('W_2015: [ ')
+    
+    # Write data for each row
+    for i in range(0,17520):
+        file.write(f" {win[i]} ")
+    
+    file.write(f' ] \n')
+
+    file.write('S_2015: [ ')
+    
+    # Write data for each row
+    for i in range(0,17520):
         file.write(f" {sol[i]} ")
     
     file.write(f' ] \n')
